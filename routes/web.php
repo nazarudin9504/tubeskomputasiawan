8beeb37149071133502f738b7c5bb73b035ec4cf
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2025-11-28 03:08:11 +0700

    update web fixx

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,43 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// kalau buka "/", arahkan ke dashboard
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
+// dashboard hanya bisa diakses setelah login & email verified
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// semua route user hanya bisa diakses user yang sudah login
 Route::middleware('auth')->group(function () {
+
+    // daftar user + jumlah akun
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+    // form tambah user
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+    // simpan user baru
     Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+    // form edit user
     Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+
+    // update user
     Route::post('/users/{id}', [UserController::class, 'update'])->name('users.update');
+
+    // hapus user
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 });
+
+// jika pakai Breeze/Jetstream, pastikan baris ini tetap ada di bawah:
+require __DIR__.'/auth.php';
