3452761792d57ada7ed21e563efa65baca438690
Nazarudin <160707783+nazarudin9504@users.noreply.github.com>
2025-11-28 02:26:03 +0700

    Revert "remove laravel breeze"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,49 +10,6 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-
-                    <!-- TABEL USER -->
-                    <div class="mt-6">
-                        <div class="overflow-x-auto">
-                            <table class="min-w-full divide-y divide-gray-200">
-                                <thead class="bg-gray-50">
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nama</th>
-                                        <th>Email</th>
-                                        <th>Terdaftar</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse($users as $user)
-                                    <tr>
-                                        <td>{{ $user->id }}</td>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ $user->created_at?->format('Y-m-d') }}</td>
-                                        <td>
-                                            <a href="{{ route('users.show', $user) }}">Lihat</a>
-                                            <a href="{{ route('users.edit', $user) }}">Edit</a>
-                                            <form action="{{ route('users.destroy', $user) }}" method="POST" class="inline-block" onsubmit="return confirm('Hapus user ini?')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit">Hapus</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @empty
-                                    <tr>
-                                        <td colspan="5">Tidak ada user.</td>
-                                    </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-                        {{ $users->links() }}
-                    </div>
-                    <!-- END TABEL USER -->
-
                 </div>
             </div>
         </div>
